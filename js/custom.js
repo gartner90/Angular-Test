d3f08eeeebe6b05b8a83580796784b9bdcd721e9
Cristian Gartner <gartner90@gmail.com>
2015-04-16 01:56:42 -0500

    Sass styles

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,10 @@
+var obJson;	
+
 $(document).ready(function() {
+	//Copy data from other site
 	var url = 'http://events.stanford.edu/2014/October/5/',
 		objEvent= '',
 		objects,
-		obJson,
 		jsonEvents;	
 	
 	$.ajax({
@@ -14,13 +16,15 @@ $(document).ready(function() {
 			mapear();
 		}
 	})
-
+	//Separate items
 	function mapear() {
 		var divN = objects.indexOf('<div id="main-content"'),
 			divN2 = objects.indexOf('<!-- Start #sidebar-second -->'),
-			divRest = divN2 - divN;
-			divSet = objects.substr(divN,divRest);
+			divRest = divN2 - divN,
+			divSet = objects.substr(divN,divRest),
+			totalItems;
 
+		//Reset syntax
 		$('.clear').html(divSet);
 		$(".postcard-link br").remove();
 		$('.postcard-link').html(function(i,h){
@@ -32,9 +36,8 @@ $(document).ready(function() {
 		$('.postcard-link').html(function(i,h){
 		    return h.replace("'", "");
 		});
-
-		var totalItems = $('.postcard-left').length;
-
+		totalItems = $('.postcard-left').length;
+		//Preparate Json object
 		$('.postcard-link').find('.postcard-left').each( function(index){
 			var titleT = $('h3', this).text(),
 				title = $.trim(titleT),
@@ -43,24 +46,40 @@ $(document).ready(function() {
 				domine = 'http://events.stanford.edu',
 				img = $('.postcard-image img', this).attr('src'),
 				domineImg = domine+img;
-				if (index === totalItems - 1) {
-			        objEvent += '{"title":' + '"' + title + '"'+ ',"date":' + '"' + date + '"' + ',"img":' + '"' + domineImg + '"}';
-			    } else {
-			    	objEvent += '{"title":' + '"' + title + '"'+ ',"date":' + '"' + date + '"' + ',"img":' + '"' + domineImg + '"},';
-			    }
+
+			if (index === totalItems - 1) {
+		        objEvent += '{"title":' + '"' + title + '"'+ ',"date":' + '"' + date + '"' + ',"img":' + '"' + domineImg + '"}';
+		    } else {
+		    	objEvent += '{"title":' + '"' + title + '"'+ ',"date":' + '"' + date + '"' + ',"img":' + '"' + domineImg + '"},';
+		    }
 		});
-		
+		//Convert strings to Json object
 		jsonEvents = '{"events":[' + objEvent + ']}';
 		obJson = JSON.parse(jsonEvents);
 		$('.clear').remove();
-		window.globJson = obJson;
 	}
-	/*setTimeout(function() {
-      console.clear();
-	}, 1800);*/
 	setTimeout(function() {
-	     console.log(globJson);
+      console.clear();
 	}, 1800);
+
+	//Swiche view
+	$( '.view-list' ).click(function() {
+		$('.type-view').attr("id","list");
+	});
+
+	$( '.view-grid' ).click(function() {
+		$('.type-view').attr("id","grid");
+	});
 	
-	 
 }); 
+
+//--Angular Events
+
+var app = angular.module('myapp', []);
+//Json object in my Angular controller
+app.controller('myController', function($scope, $timeout) {
+    $timeout( function(){ 
+    	var json = obJson;
+    	$scope.myscope = json;
+    }, 1800);//Time while load items
+});
